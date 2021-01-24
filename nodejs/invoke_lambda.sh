2ec6fcabab69d4485a9879e6f3978721f55c857f
rgirodon <remy.girodon@gmail.com>
2021-01-24 21:27:20 +0100

    Add SQS => Lambda example

--- nodejs/invoke_lambda.sh
+++ nodejs/invoke_lambda.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 aws lambda invoke --function-name rgirodon_lambda_nodejs_helloworld  --payload file://lambda_event.json out.json
+
+aws lambda create-event-source-mapping --function-name ProcessSQSRecord --batch-size 10 --event-source-arn arn:aws:sqs:us-east-1:343830583259:ProcessRecords_Queue
+
+aws lambda list-event-source-mappings --function-name ProcessSQSRecord --event-source-arn arn:aws:sqs:us-east-1:343830583259:ProcessRecords_Queue
+
+aws lambda delete-event-source-mapping --uuid 94a2f9d8-c240-4be1-a7e4-955cf21237b4  
