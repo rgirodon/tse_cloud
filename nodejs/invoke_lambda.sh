a7898492a2b24813b9601e5ea5d9800bb061d9cb
rgirodon <remy.girodon@gmail.com>
2022-10-28 11:31:11 +0200

    Add aws cli v2 parameter for base64

--- nodejs/invoke_lambda.sh
+++ nodejs/invoke_lambda.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-aws lambda invoke --function-name rgirodon_lambda_nodejs_helloworld  --payload file://lambda_event.json out.json
+aws lambda invoke --function-name rygn-first-lambda --payload '{"name" : "Rémy"}' out.json
 
-aws lambda create-event-source-mapping --function-name ProcessSQSRecord --batch-size 10 --event-source-arn arn:aws:sqs:us-east-1:343830583259:ProcessRecords_Queue
+aws lambda invoke --function-name rygn-first-lambda --payload file://lambda_event.json out.json
 
-aws lambda list-event-source-mappings --function-name ProcessSQSRecord --event-source-arn arn:aws:sqs:us-east-1:343830583259:ProcessRecords_Queue
+# with aws cli v2
+# --cli-binary-format raw-in-base64-out
 
-aws lambda delete-event-source-mapping --uuid 94a2f9d8-c240-4be1-a7e4-955cf21237b4  
+aws lambda create-event-source-mapping --function-name ProcessSQSRecord --batch-size 10 --event-source-arn arn:aws:sqs:us-east-1:150282616767:rygn-first-sqs
+
+aws lambda list-event-source-mappings --function-name ProcessSQSRecord --event-source-arn arn:aws:sqs:us-east-1:150282616767:rygn-first-sqs
+
+aws lambda delete-event-source-mapping --uuid ac8802ba-0b30-4a1a-90bd-250fd5576c43
